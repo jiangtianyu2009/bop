54a07ec6f60023d54e95700e373cd3a31faace4c
jiangtianyu <jiangtianyu2009@hotmail.com>
2017-08-23 15:39:05 +0800

    Reverse Integer Complete
    Reverse complete.

--- Reverse Integer/reverseinteger.py
+++ Reverse Integer/reverseinteger.py
@@ -12,3 +12,34 @@ Note:
 The input is assumed to be a 32-bit signed integer.
 Your function should return 0 when the reversed integer overflows.
 '''
+
+
+def reverse(p):
+    """
+    :type x: int
+    :rtype: int
+    """
+    if p > 0:
+        x = p
+    else:
+        x = -p
+    y = 0
+
+    while x != 0:
+        n = x % 10
+        y = y * 10 + n
+        x = x // 10
+
+    if p > 0:
+        retu = y
+    else:
+        retu = -y
+
+    if retu > 2147483647 or retu < -2147483648:
+        return 0
+    else:
+        return retu
+
+
+if __name__ == '__main__':
+    print(reverse(123))
