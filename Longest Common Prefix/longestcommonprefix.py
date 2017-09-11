55779f2e753ab15132528c057038d0649254f859
jiangtianyu <jiangtianyu2009@hotmail.com>
2017-09-11 22:53:34 +0800

    Update Function Defin
    Update function definition.

--- Longest Common Prefix/longestcommonprefix.py
+++ Longest Common Prefix/longestcommonprefix.py
@@ -5,3 +5,9 @@ Leetcode
 
 Write a function to find the longest common prefix string amongst an array of strings.
 '''
+
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
