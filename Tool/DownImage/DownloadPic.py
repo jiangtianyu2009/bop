e00cce45232a443eec699e8cc14d361ff9726383
jiangtianyu <jiangtianyu2008@gmail.com>
2018-06-04 10:09:11 +0800

    Add Image Download
    Add image download scripts.

---
+++ Tool/DownImage/DownloadPic.py
@@ -0,0 +1,33 @@
+import os
+import re
+import shutil
+import urllib.request
+
+import bs4
+import requests
+
+base_url = 'http://www.javlibrary.com/tw/vl_searchbyid.php?keyword='
+filterWord = "video_jacket_img"
+srcDirList = [r'C:\jty\xunlei']
+
+for srcDir in srcDirList:
+    filenames = os.listdir(srcDir)
+    for filename in filenames:
+        preFileName = filename.split(".")[0]
+        if preFileName[-1] == "A" or preFileName[-1] == "B" or preFileName[-1] == "C":
+            preFileName = preFileName[0:len(preFileName) - 1]
+        destPicName = srcDir + os.sep + preFileName + '.jpg'
+        if os.path.isfile(destPicName):
+            print(destPicName + ' already here.\n')
+        else:
+            full_url = base_url + preFileName
+            response = requests.get(full_url)
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            try:
+                imgsrc = 'http:' + soup.find(id=filterWord)['src']
+                print(preFileName + "\n" + imgsrc)
+                print(destPicName + "\n")
+                if not os.path.isfile(destPicName):
+                    urllib.request.urlretrieve(imgsrc, destPicName)
+            except:
+                print('!!!!!!!!!!!!!! Can not find picture of ' + filename + '\n')
