4a92ece3b4bb5296c4ebda690a9e4c4dd2083d44
jiangtianyu <jiangtianyu2008@gmail.com>
2020-07-21 14:51:24 +0800

    Update First_Unique_Character_in_a_String.py

--- Leetcode/First_Unique_Character_in_a_String.py
+++ Leetcode/First_Unique_Character_in_a_String.py
@@ -1,7 +1,32 @@
-from typing import List
-
-
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
+    def firstUniqChar(self, s):
         dict_char = {}
         for char in s:
+            if char in dict_char:
+                dict_char[char] += 1
+            else:
+                dict_char[char] = 1
+        print(dict_char)
+        key = self.firstUniqCharinDict(dict_char)
+        index = self.getIndex(key, s)
+        return index
+
+    def firstUniqCharinDict(self, dict_char):
+        for key in dict_char:
+            if dict_char[key] == 1:
+                print(key)
+                return key
+        return None
+
+    def getIndex(self, key, s):
+        if key is None:
+            return -1
+        else:
+            index = s.index(key)
+            print(index)
+            return index
+
+
+if __name__ == '__main__':
+    s = "loveleetcode"
+    Solution().firstUniqChar(s)
