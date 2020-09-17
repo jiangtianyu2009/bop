274801d920ef544008c1bb81575689e3bc964eed
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-09-17 15:57:12 +0800

    Update SortUtility.py

--- Comicbook/SortUtility.py
+++ Comicbook/SortUtility.py
@@ -6,9 +6,7 @@ class Sort:
         pass
 
     def bubbleSort(self, nums):
-        sortedlength = 0
-        for k, num in enumerate(nums[:len(nums) - sortedlength]):
-            sortedlength = k + 1
+        for n in nums:
             for i, num in enumerate(nums):
                 if i == 0:
                     pass
