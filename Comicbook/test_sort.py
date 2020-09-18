e18e6042d782698ed47a358a8c4a4521110694ef
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-09-18 15:57:00 +0800

    Update test_sort.py

--- Comicbook/test_sort.py
+++ Comicbook/test_sort.py
@@ -1,33 +1,33 @@
-from SortUtility import Sort
+from SortUtility import SortUtility
 import random
 import datetime
+import copy
 
 
 def test_sort_result():
     unsortedlist = []
-    for i in range(20):
+    for i in range(10):
         unsortedlist.append(random.randint(1, 99))
-    print(unsortedlist)
-    list_bubble = unsortedlist
-    list_quick = unsortedlist
-    print(list_bubble)
-    print(list_quick)
-    # assert list_bubble == list_quick
+    list_bubble = copy.deepcopy(unsortedlist)
+    list_quick = copy.deepcopy(unsortedlist)
+    print('list_bubble: ', list_bubble)
+    print('list_quick : ', list_quick)
     # Execute sort
-    list_bubble_sorted = Sort().bubbleSort(list_bubble)
-    print(list_bubble_sorted)
-    list_quick_sorted = Sort().quickSort(list_quick)
-    print(list_quick_sorted)
-    # assert list_bubble_sorted == list_quick_sorted
+    list_bubble_sorted = SortUtility().bubbleSort(list_bubble)
+    print('list_bubble_sorted: ', list_bubble_sorted)
+    list_quick_sorted = SortUtility().quickSort(list_quick)
+    print('list_quick_sorted : ', list_quick_sorted)
+    assert list_quick_sorted == list_bubble_sorted
 
 
 if __name__ == "__main__":
-    unsortedlist = []
-    for i in range(20):
-        unsortedlist.append(random.randint(1, 99))
-    # Quick sort time cost
-    quick_time_start = datetime.datetime.now()
-    sortedlist = Sort().bubbleSort(unsortedlist)
-    quick_time_end = datetime.datetime.now()
-    quick_time_delta = quick_time_end - quick_time_start
-    print(quick_time_delta.total_seconds())
+    test_sort_result()
+    # unsortedlist = []
+    # for i in range(20):
+    #     unsortedlist.append(random.randint(1, 99))
+    # # Quick sort time cost
+    # quick_time_start = datetime.datetime.now()
+    # sortedlist = SortUtility().bubbleSort(unsortedlist)
+    # quick_time_end = datetime.datetime.now()
+    # quick_time_delta = quick_time_end - quick_time_start
+    # print(quick_time_delta.total_seconds())
