7ccdb69d76df8861868723cd2ac0e121ae045ea9
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-09-21 15:26:24 +0800

    Update quick_sort_issue.py

--- Comicbook/quick_sort_issue.py
+++ Comicbook/quick_sort_issue.py
@@ -1,11 +1,15 @@
+import random
+
+
 class Sort:
-    def quickSort(self, unsortedlist):
-        if len(unsortedlist) <= 1:
-            return unsortedlist
-        pivot = unsortedlist[0]
-        unsortedlist.remove(unsortedlist[0])
+    def quickSort(self, nums):
+        nums = nums[:]
+        if len(nums) <= 1:
+            return nums
+        pivot = nums[random.randint(0, len(nums) - 1)]
+        nums.remove(pivot)
         left, right = [], []
-        for num in unsortedlist:
+        for num in nums:
             if num < pivot:
                 left.append(num)
             else:
@@ -15,6 +19,7 @@ class Sort:
 
 if __name__ == "__main__":
     a = [76, 76, 65, 72, 58, 64, 82, 3, 22, 31]
+    print(Sort().quickSort(a))
     print(Sort().quickSort(a))
     print(Sort().quickSort(a))
     print(Sort().quickSort(a))
