2ac0af6543de99b747007f563aa23ee26ea849c6
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-09-28 13:20:45 +0800

    Update 3Sum.py

--- Leetcode/3Sum.py
+++ Leetcode/3Sum.py
@@ -1,25 +1,46 @@
 class Solution:
     def threeSum(self,  nums):
-        temp_list = []
+        if len(nums) < 3:
+            return []
+        if self.is_all_zero(nums):
+            return [[0, 0, 0]]
+        retList = []
         nums = sorted(nums)
         for index, num in enumerate(nums):
-            if nums[index + 1] == nums[index]:
-                break
-            target = 0 - num
-            two_nums = nums[index + 1:]
-            numDict = {}
-            for index, two_num in enumerate(two_nums):
-                if (target - two_num) not in numDict:
-                    numDict[two_num] = index
-                else:
-                    temp_list.append([num, target - two_num, two_nums[index]])
-        temp_set = set(tuple(s) for s in temp_list)
-        new_list = [list(t) for t in temp_set]
-        print(new_list)
-        return new_list
+            retList = retList + self.twoSumPro(nums[index + 1:], -num)
+        return self.deDup(sorted(retList))
+
+    def twoSumPro(self, nums, target):
+        numDict = {}
+        retList = []
+        for index, num in enumerate(nums):
+            if (target - num) not in numDict:
+                numDict[num] = index
+            else:
+                retList.append(sorted([target - num, num, -target]))
+        return self.deDup(retList)
+
+    def deDup(self, ret_list):
+        dedup_list = []
+        for i, nums in enumerate(ret_list):
+            if i != 0 and ret_list[i] == base_nums:
+                pass
+            else:
+                base_nums = nums
+                dedup_list.append(nums)
+        return dedup_list
+
+    def is_all_zero(self, nums):
+        zero_flag = True
+        for num in nums:
+            if num == 0:
+                pass
+            else:
+                zero_flag = False
+        return zero_flag
 
 
 if __name__ == '__main__':
-    nums1 = [-1, 0, 1, 2, -1, -4]
-    nums2 = [0, -4, -1, -4, -2, -3, 2]
-    Solution().threeSum(nums2)
+    assert Solution().threeSum([-1, 0, 1, 2, -1, -4]
+                               ) == [[-1, -1, 2], [-1, 0, 1]]
+    assert Solution().threeSum([0, 0, 0, 0, 0, 0, 0]) == [[0, 0, 0]]
