e40e7a29d6485d929522ed6f05ec22e545d3764e
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2022-08-22 23:17:27 +0800

    Update PicAndVideoRename.py

--- Utility/DownImage/PicAndVideoRename.py
+++ Utility/DownImage/PicAndVideoRename.py
@@ -2,7 +2,7 @@ import os
 import re
 
 p = re.compile(r'(\D+\d+)(\w*)(.\w+)')
-dstDirList = [r'H:\temp\TC']
+dstDirList = [r'H:\\Download\TC']
 
 for distDir in dstDirList:
     filenames = os.listdir(distDir)
