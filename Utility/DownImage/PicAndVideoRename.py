498b5e373360f63cb9ec9b97c0dbeb378bd75e41
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2023-07-26 21:58:41 +0800

    Update PicAndVideoRename.py

--- Utility/DownImage/PicAndVideoRename.py
+++ Utility/DownImage/PicAndVideoRename.py
@@ -3,30 +3,35 @@ import re
 
 p = re.compile(r'(\D+\d+)(\w*)(.\w+)')
 dstDirList = [r'H:\Download\TC', r'G:\Download\TC',
-              r'F:\Download\TC', r'E:\Download\TC']
+              r'F:\Download\TC', r'D:\Download\TC',
+              r'E:\Download\TC']
 
 for distDir in dstDirList:
-    filenames = os.listdir(distDir)
-    print(filenames)
-    if 'desktop.ini' in filenames:
-        filenames.remove('desktop.ini')
-    if 'Thumbs.db' in filenames:
-        filenames.remove('Thumbs.db')
-    for filenamepref in filenames:
-        if filenamepref.find('_') > 0:
-            filenameprefit = filenamepref[filenamepref.index('_'):]
-        else:
-            filenameprefit = filenamepref
-        filenamepost = filenameprefit.replace('-', '').replace('_', '')\
-            .replace(' ', '').replace('.1080p', '').replace('.720p', '')\
-            .replace('[thz.la]', '').replace('[Thz.la]', '').replace('[HD]', '')\
-            .replace('cd1', 'A').replace('cd2', 'B').replace('hhd800.com@', '')
-        fhalf = p.search(filenamepost).group(1).upper()
-        mhalf = p.search(filenamepost).group(2).upper()
-        lhalf = p.search(filenamepost).group(3).lower()
-        if mhalf == "A" or mhalf == "B" or mhalf == "C":
-            distname = fhalf + mhalf + lhalf
-        else:
-            distname = fhalf + lhalf
-        print(distname)
-        os.rename(distDir + os.sep + filenamepref, distDir + os.sep + distname)
+    try:
+        filenames = os.listdir(distDir)
+        print(filenames)
+        if 'desktop.ini' in filenames:
+            filenames.remove('desktop.ini')
+        if 'Thumbs.db' in filenames:
+            filenames.remove('Thumbs.db')
+        for filenamepref in filenames:
+            if filenamepref.find('_') > 0:
+                filenameprefit = filenamepref[filenamepref.index('_'):]
+            else:
+                filenameprefit = filenamepref
+            filenamepost = filenameprefit.replace('-', '').replace('_', '')\
+                .replace(' ', '').replace('.1080p', '').replace('.720p', '')\
+                .replace('[thz.la]', '').replace('[Thz.la]', '').replace('[HD]', '')\
+                .replace('cd1', 'A').replace('cd2', 'B').replace('hhd800.com@', '')
+            fhalf = p.search(filenamepost).group(1).upper()
+            mhalf = p.search(filenamepost).group(2).upper()
+            lhalf = p.search(filenamepost).group(3).lower()
+            if mhalf == "A" or mhalf == "B" or mhalf == "C":
+                distname = fhalf + mhalf + lhalf
+            else:
+                distname = fhalf + lhalf
+            print(distname)
+            os.rename(distDir + os.sep + filenamepref,
+                      distDir + os.sep + distname)
+    except:
+        pass
